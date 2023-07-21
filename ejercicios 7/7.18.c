43892217d7ade0d962500682eca495e737602975
FreddyA.SurielG.20230801 <134118695+FreddyASuriel@users.noreply.github.com>
2023-07-21 14:16:05 -0400

    Update 7.18.c

--- ejercicios 7/7.18.c
+++ ejercicios 7/7.18.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-
-/* Cadena invertida resuelta en forma recursiva. */
-
 void inverso(char *); /* Prototipo de función. */
 
 int main(void)
@@ -15,13 +12,7 @@ int main(void)
 }
 
 void inverso(char *cadena)
-/* La función inverso obtiene precisamente el inverso de la cadena. La solución
-➥presentada es simple, pero para comprenderla es necesario tener conocimientos
-➥tanto de pilas como de recursividad. Observa que mientras no se encuentre el
-➥caracter de terminación de la cadena, se llama a la función recursiva con
-➥el apuntador al siguiente caracter de la cadena. Por otra parte, queda
-➥pendiente de ejecutar —almacenado en una pila— el caracter al cual apunta
-➥*cadena. */
+
 {
     if (cadena[0] != '\0')
     {
