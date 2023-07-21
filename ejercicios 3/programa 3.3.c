dd3a8ce43eed9372e8a9f056c25d9340dff5aa05
FreddyA.SurielG.20230801 <134118695+FreddyASuriel@users.noreply.github.com>
2023-07-21 13:56:21 -0400

    Update programa 3.3.c

--- ejercicios 3/programa 3.3.c
+++ ejercicios 3/programa 3.3.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-
-/* Suma pagos.
-El programa, al recibir como datos un conjunto de pagos realizados en el ultimo mes, obtiene la suma de los mismos.
-  PAG y SPA: variables de tipo real*/
 void main(void)
 
 {
@@ -11,13 +7,12 @@ void main(void)
   printf("Ingrese el primer pago:\t");
   scanf("%f", &PAG);
   while (PAG)
-    /* observa que la condicion es verdadera mientras el pago es diferente de cero.*/
+  
     {
       SPA = SPA + PAG;
       printf("Ingrese el siguiente pago:\t ");
       scanf("%f", &PAG);
       while (PAG)
-        /* observa que la posicion que modifica la condicion es una lectura*/
      
   printf("\nEl total de pagos del mes es: %.2f", SPA);
     }
