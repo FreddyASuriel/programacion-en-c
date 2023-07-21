6de90e8745afb87436a55d1d0356abd9d5c62b18
FreddyA.SurielG.20230801 <134118695+FreddyASuriel@users.noreply.github.com>
2023-07-21 14:27:36 -0400

    Update 9.4.c

--- ejercicios 9/9.4.c
+++ ejercicios 9/9.4.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-
-/* Archivos y cadenas de caracteres.
-El programa lee cadenas de caracteres de un archivo. */
-
 int main(void) {
     char cad[50]; 
     FILE *ap;
     if ((ap=fopen ("arc.txt", "r")) != NULL) {
-        /* Se abre el archivo para lectura y se verifica si se abrió correctamente. */
         while (fgets(cad, 50, ap) != NULL) {
-            /* Mientras no se detecte el fin de archivo se siguen leyendo cadenas de caracteres. */
             puts(cad);
         }
-        /* Despliega la cadena en la pantalla. */
         fclose(ap);
     }
     else {
