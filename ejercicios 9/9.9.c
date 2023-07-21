3eef855650b285b9796f18f485f9681ac9c2cbee
FreddyA.SurielG.20230801 <134118695+FreddyASuriel@users.noreply.github.com>
2023-07-21 14:31:27 -0400

    Update 9.9.c

--- ejercicios 9/9.9.c
+++ ejercicios 9/9.9.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-/* Alumnos.
-El programa lee bloques —variables de tipo estructura alumno— de un archivo ➥de acceso directo. */
 typedef struct { 
 int matricula; 
 char nombre[20];
@@ -9,8 +7,6 @@ float promedio;
 } alumno;
 
 void lee(FILE *);
-/* Declaración de la estructura alumno. */
-/* Prototipo de función. */
 void main(void)
 {
 FILE *ar;
@@ -19,10 +15,9 @@ if ((ar = fopen ("ad1.dat", "r")) != NULL)
 printf("\nEl archivo no se puede abrir"); fclose(ar);
 }
 void lee(FILE *ap)
-/* Esta función se utiliza para leer bloques de un archivo de acceso directo. */ {
+{
 alumno alu;
 fread(&alu, sizeof(alumno), 1, ap);
-/* Observa que la función fread tiene los mismos argumentos que la función ➥fwrite del programa anterior. También es importante tomar nota de que cuando ➥tenemos que leer los registros de un archivo utilizando una estructura ➥repetitiva como el while, debemos realizar una primera lectura antes de ➥ingresar al ciclo y luego las siguientes dentro del ciclo, pero como última ➥instrucción del mismo. Esto se debe a que la lógica que siguen las ➥instrucciones fwrite y fread es moverse y leer o escribir, según sea el caso. ➥Si no lo hiciéramos de esta forma, terminaríamos escribiendo la información ➥del último registro dos veces. Vamos a realizar un ejemplo sencillo para ➥comprobar esto. */
 while (!feof(ap)) {
 printf("\nMatrícula: %d", alu.matricula); 
   printf("\tCarrera: %d", alu.carrera);
