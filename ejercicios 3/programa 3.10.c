2b50c916ca16acfc0ecb20c7fc8b76a2067de5cb
FreddyA.SurielG.20230801 <134118695+FreddyASuriel@users.noreply.github.com>
2023-07-21 13:51:23 -0400

    Update programa 3.10.c

--- ejercicios 3/programa 3.10.c
+++ ejercicios 3/programa 3.10.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-
-/* pares e impares.
-el programa al recibir como dato N numeros enteros, obtiene la suma de los numeros pared y calcula el promedio de los impares.
-
-  I, N, NUM, SPA, SIM, CIM: variable de tipos enteros. */
-
 void main(void)
 {
   int I, N, NUM, SPA = 0, SIM = 0, CIM = 0;
