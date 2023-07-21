87bcd09802407d15bd2943dd22c70f35ca9274d5
FreddyA.SurielG.20230801 <134118695+FreddyASuriel@users.noreply.github.com>
2023-07-21 14:14:56 -0400

    Update 7.17.c

--- ejercicios 7/7.17.c
+++ ejercicios 7/7.17.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-
-/* Cadena invertida.
-El programa obtiene la cadena invertida. */
-
 char *inverso(char *); /* Prototipo de función. */
 
 int main(void)
@@ -18,15 +14,12 @@ int main(void)
 }
 
 char *inverso(char *cadena)
-/* La función calcula el inverso de una cadena y regresa el resultado al programa principal. */
 {
     int i = 0, j, lon;
     char cad;
     lon = strlen(cadena);
     j = lon - 1;
     while (i < ((lon - 1) / 2))
-        /* Observa que el reemplazo de los caracteres se debe realizar solamente hasta la mitad de la cadena. */
-    {
         cad = cadena[i];
         cadena[i] = cadena[j];
         cadena[j] = cad;
